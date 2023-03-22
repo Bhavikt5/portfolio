61a6927762eef488fa1100b3511d13682498fa18
bhavikt5 <91686940+Bhavikt5@users.noreply.github.com>
2023-03-22 13:23:26 +0530

    portfolio api updated

--- pages/api/workportfolio.js
+++ pages/api/workportfolio.js
@@ -6,6 +6,24 @@ const work = [
     anchorLink: "https://project6widgetor.vercel.app/",
   },
   {
+    imageUrl: "/datascene.png",
+    title: "CRM Website",
+    stack: "HTML, CSS, JavaScript, Jquery, Bootstrap",
+    anchorLink: "https://project5widgetor-dasboard.vercel.app/homepage.html",
+  },
+  {
+    imageUrl: "/utsava.png",
+    title: "Ecommerce Website",
+    stack: "HTML, CSS, JavaScript",
+    anchorLink: "https://bhavikt5.github.io/utsava/",
+  },
+  {
+    imageUrl: "/construction.png",
+    title: "Construction Website",
+    stack: "HTML, CSS, JavaScript, SCSS",
+    anchorLink: "https://bhavikt5.github.io/Construction-Site/",
+  },
+  {
     imageUrl: "/widgetorweb.png",
     title: "CRM Website",
     stack: "HTML, CSS, JavaScript, Jquery, Bootstrap",
